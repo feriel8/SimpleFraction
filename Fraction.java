b066b184ca70a363dc8923ead6389b15ad9ba502
Utilisateur <user@debian.arcahe.ovh>
2020-09-25 09:52:04 +0200

    first commit

---
+++ Fraction.java
@@ -0,0 +1,22 @@
+
+public class Fraction {
+
+		 private int numerateur;
+		 private int denominateur;
+
+
+		/**Constructeur**/
+		 public Fraction(){
+		  this.numerateur=0;
+		  this.denominateur=1;
+		 }
+		 public Fraction(int unNumerateur,int unDenominateur){
+		  this.numerateur=unNumerateur;
+		  this.denominateur=unDenominateur;
+		 }
+		 public Fraction(int unNumerateur){
+		  this.numerateur=unNumerateur;
+		  this.denominateur=1;
+		 }
+
+}
